34fb186ff8c4a1cf27389975acb5a74deeb0045f
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-10-20 00:09:34 -0300

    correct styling

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,4 +1,3 @@
-import { NextNavButton, PrevNavButton } from 'components/NavButton/index'
 import { SearchBox } from 'components/SearchBox/index'
 import { useRouter } from 'node_modules/next/router'
 import React from 'react'
@@ -13,17 +12,17 @@ export const NavBar = ({ back }) => {
   }
   return (
     <>
-      <nav className='flex flex-row items-baseline w-full px-3 pb-3 bg-rose-600'>
+      <nav className='flex flex-row items-baseline w-full justify-center px-3 pb-3 gap-60 bg-rose-600 relative'>
         {back && (
           <button
             onClick={goBack}
-            className='text-lg md:text-2xl no-underline text-white hover:no-underline hover:text-white justify-self-start'>
+            className='text-lg md:text-2xl no-underline text-white hover:no-underline hover:text-white justify-self-start absolute top-0 left-0'>
             Go back
           </button>
         )}
         <button
           onClick={goHome}
-          className='text-lg md:text-2xl no-underline text-white hover:no-underline hover:text-white mx-auto'>
+          className='text-lg md:text-2xl no-underline text-white hover:no-underline hover:text-white'>
           Home
         </button>
       </nav>
